67b42da42cff0e3974ec0b416e2350fe0200829d
antoshkaaaa2000 <148551570+antoshkaaaa2000@users.noreply.github.com>
2024-06-11 13:25:04 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,38 +1,7 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE abiword PUBLIC "-//ABISOURCE//DTD AWML 1.0 Strict//EN" "http://www.abisource.com/awml.dtd">
-<abiword template="false" xmlns:ct="http://www.abisource.com/changetracking.dtd" xmlns:fo="http://www.w3.org/1999/XSL/Format" xmlns:math="http://www.w3.org/1998/Math/MathML" xid-max="12" xmlns:dc="http://purl.org/dc/elements/1.1/" fileformat="1.1" xmlns:svg="http://www.w3.org/2000/svg" xmlns:awml="http://www.abisource.com/awml.dtd" xmlns="http://www.abisource.com/awml.dtd" xmlns:xlink="http://www.w3.org/1999/xlink" version="3.0.5" xml:space="preserve" props="dom-dir:ltr; document-footnote-restart-section:0; document-endnote-type:numeric; document-endnote-place-enddoc:1; document-endnote-initial:1; lang:ru-RU; document-endnote-restart-section:0; document-footnote-restart-page:0; document-footnote-type:numeric; document-footnote-initial:1; document-endnote-place-endsection:0">
-<!-- ======================================================================== -->
-<!-- This file is an AbiWord document.                                        -->
-<!-- AbiWord is a free, Open Source word processor.                           -->
-<!-- More information about AbiWord is available at http://www.abisource.com/ -->
-<!-- You should not edit this file by hand.                                   -->
-<!-- ======================================================================== -->
-
-<metadata>
-<m key="abiword.date_last_changed">Tue Jun 11 19:09:36 2024
-</m>
-<m key="abiword.generator">AbiWord</m>
-<m key="dc.date">Tue Jun 11 19:09:36 2024
-</m>
-<m key="dc.format">application/x-abiword</m>
-</metadata>
-<rdf>
-</rdf>
-<history version="1" edit-time="300" last-saved="1718122176" uid="4cc3a3cc-280c-11ef-8a9e-88227aecdb94">
-<version id="1" started="1718122176" uid="ff6a666e-280c-11ef-8a9e-88227aecdb94" auto="0" top-xid="12"/>
-</history>
-<styles>
-<s type="P" name="Normal" basedon="" followedby="Current Settings" props="font-family:Liberation Serif; margin-top:0pt; font-variant:normal; margin-left:0pt; text-indent:0in; widows:2; font-style:normal; font-weight:normal; text-decoration:none; color:000000; line-height:1.0; text-align:left; margin-bottom:0pt; text-position:normal; margin-right:0pt; bgcolor:transparent; font-size:12pt; font-stretch:normal"/>
-</styles>
-<pagesize pagetype="A4" orientation="portrait" width="8.267717" height="11.692913" units="in" page-scale="1.000000"/>
-<section xid="11">
-<p style="Normal" xid="12"><c>sudo apt update</c></p>
-<p style="Normal" xid="2" props="text-align:left; dom-dir:ltr"><c>sudo apt install git </c><c props="font-family:Liberation Serif; font-size:12pt; color:000000; text-decoration:none; text-position:normal; font-weight:normal; font-style:normal; lang:ru-RU">dkms</c><c> xserver-xorg-video-intel mesa-utils</c></p>
-<p style="Normal" xid="3" props="text-align:left; dom-dir:ltr"><c>sudo add-apt-repository ppa:oibaf/graphics-drivers</c></p>
-<p style="Normal" xid="6" props="text-align:left; dom-dir:ltr"><c>sudo apt full-upgrade</c></p>
-<p style="Normal" xid="7" props="text-align:left; dom-dir:ltr"><c>sudo add-apt-repository ppa:kelebek333/kablosuz</c></p>
-<p style="Normal" xid="8" props="text-align:left; dom-dir:ltr"><c>sudo apt install rtl8821ce-dkms</c></p>
-<p style="Normal" xid="9" props="text-align:left; dom-dir:ltr"><c>sudo reboot</c></p>
-<p style="Normal" xid="10" props="text-align:left; dom-dir:ltr"><c></c></p>
-</section>
-</abiword>
+sudo apt update
+sudo apt install git dkms xserver-xorg-video-intel mesa-utils wget
+sudo add-apt-repository ppa:oibaf/graphics-drivers
+sudo apt full-upgrade
+sudo add-apt-repository ppa:kelebek333/kablosuz
+sudo apt install rtl8821ce-dkms
+sudo reboot
---
+++ install2.sh
@@ -0,0 +1,5 @@
+wget https://cdn.akamai.steamstatic.com/client/installer/steam.deb
+sudo dpkg -i steam_latest.deb
+wget https://github.com/Alex313031/thorium/releases/download/M124.0.6367.218/thorium-browser_124.0.6367.218_AVX.deb
+sudo spkg -i thorium-browser_124.0.6367.218_AVX.deb
+sudo apt install gparted vlc qbittorrent
